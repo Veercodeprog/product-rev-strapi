6309b757d8fa040a2b45d5b202f3253aca56b7f7
veerpratapsinghtomar <Veerpratap945050@gmail.com>
2023-05-23 18:56:07 +0530

    host added

--- config/server.js
+++ config/server.js
@@ -1,5 +1,5 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
+  host: env('HOST', 'https://product-rev-strapi.vercel.app'),
   port: env.int('PORT', 1337),
   app: {
     keys: env.array('APP_KEYS'),
