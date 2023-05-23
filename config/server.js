5a37f7f5ec6c39772760d3d550dba1fe2f895628
veerpratapsinghtomar <Veerpratap945050@gmail.com>
2023-05-23 19:05:08 +0530

    fix hostname

--- config/server.js
+++ config/server.js
@@ -1,5 +1,5 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', 'https://product-rev-strapi.vercel.app'),
+  host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   app: {
     keys: env.array('APP_KEYS'),
