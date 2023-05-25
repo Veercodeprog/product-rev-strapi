810ae633cef8d0250376aac75fa297a0aba9a7ac
veerpratapsinghtomar <Veerpratap945050@gmail.com>
2023-05-25 14:12:05 +0530

    latest

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,19 +1,6 @@
 module.exports = [
   'strapi::errors',
-     {
-        name: 'strapi::security',
-        config: {
-          contentSecurityPolicy: {
-            useDefaults: true,
-            directives: {
-              'connect-src': ["'self'", 'https:'],
-              'img-src': ["'self'", 'data:', 'blob:', 'res.cloudinary.com'],
-              'media-src': ["'self'", 'data:', 'blob:', 'res.cloudinary.com'],
-              upgradeInsecureRequests: null,
-            },
-          },
-        },
-      },
+  'strapi::security',
   'strapi::cors',
   'strapi::poweredBy',
   'strapi::logger',
